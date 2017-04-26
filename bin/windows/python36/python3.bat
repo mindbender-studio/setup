f8ba5502f551970500ae68979e67e4799c15761b
Marcus Ottosson <konstruktion@gmail.com>
2017-04-26 09:46:58 +0100

    Add PyQt5

--- bin/windows/python36/python3.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-rem Avoid polluting the active environment
-rem by launching a separate subshell and passing it
-rem the modified environment. It vanishes upon exit.
-cmd /C %~dp0_env.bat %*
