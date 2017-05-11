f9bf1118cb2cae777c322b2a9c405dfe0017b8ef
Marcus Ottosson <konstruktion@gmail.com>
2017-05-11 16:41:46 +0100

    Enable external shortcut to mb-gui

--- mb-gui.bat
+++ mb-gui.bat
@@ -1,13 +1,13 @@
 @echo off
 
 :: Establish an environment
-call mb-env
+call %~dp0mb-env
 
 :: Take the user through initial set-up
 if "%MINDBENDER_MONGO%"=="" call mb-login
 
 :: Verify the address given by the user
-cmd /c mb-test
+cmd /c %~dp0mb-test
 
 if "%ERRORLEVEL%"=="0" python -u -m launcher --root %MINDBENDER_PROJECTS%
 
